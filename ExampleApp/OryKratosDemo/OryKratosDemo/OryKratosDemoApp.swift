2104b2c349d1d32f5e46ec054013593654bcb232
Michał Mańkus <ptasigryp@gmail.com>
2026-03-25 16:19:45 +0100

    chore(all): xcode example ios app, update gitignore

---
+++ ExampleApp/OryKratosDemo/OryKratosDemo/OryKratosDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  OryKratosDemoApp.swift
+//  OryKratosDemo
+//
+//  Created by Michal Mańkus on 25/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct OryKratosDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
